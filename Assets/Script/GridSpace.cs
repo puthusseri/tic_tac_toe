f304189ad77b1369e216af34af9c166de187c4a3
puthusseri <vyshakputhusseri@gmail.com>
2018-11-24 19:26:03 +0530

    grid can be selected

--- Assets/Script/GridSpace.cs
+++ Assets/Script/GridSpace.cs
@@ -5,5 +5,14 @@ using UnityEngine.UI;
 
 public class GridSpace : MonoBehaviour {
 
+	public Button button;
+	public Text buttonText;
+	public string playerSide;
 	
+	public void SetSpace () {
+		
+		buttonText.text = playerSide;
+		button.interactable = false;
+		
+	}
 }
