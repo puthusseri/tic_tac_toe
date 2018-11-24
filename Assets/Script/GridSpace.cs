5819b92362bb1b8fd8e3cf073ad3d8e19c408365
puthusseri <vyshakputhusseri@gmail.com>
2018-11-24 20:30:38 +0530

    palyer side was ok

--- Assets/Script/GridSpace.cs
+++ Assets/Script/GridSpace.cs
@@ -1,18 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 using UnityEngine.UI;
 
 public class GridSpace : MonoBehaviour {
 
-	public Button button;
-	public Text buttonText;
-	public string playerSide;
-	
-	public void SetSpace () {
-		
-		buttonText.text = playerSide;
-		button.interactable = false;
-		
-	}
+    public Button button;
+    public Text buttonText;
+
+    private GameController gameController;
+
+    public void SetGameControllerReference (GameController controller)
+    {
+        gameController = controller;
+    }
+
+    public void SetSpace ()
+    {
+        buttonText.text = gameController.GetPlayerSide();
+        button.interactable = false;
+        gameController.EndTurn();
+    }
 }
