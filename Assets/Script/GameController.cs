4ae804a3201eb340859e2e6458ef0130a3b98bcc
puthusseri <vyshakputhusseri@gmail.com>
2018-11-24 20:59:27 +0530

    chance of being the game as draw was also included

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -9,12 +9,15 @@ public class GameController : MonoBehaviour {
     public Text gameOverText;
 
     private string playerSide;
+    private int moveCount;
 
     void Awake ()
+
     {
         SetGameControllerReferenceOnButtons();
         playerSide = "X";
         gameOverPanel.SetActive(false);
+        moveCount = 0;
     }
 
     void SetGameControllerReferenceOnButtons ()
@@ -32,7 +35,7 @@ public class GameController : MonoBehaviour {
 
     public void EndTurn ()
     {
-
+        moveCount++;
         if (buttonList [0].text == playerSide && buttonList [1].text == playerSide && buttonList [2].text == playerSide)
         {
             GameOver();
@@ -67,10 +70,17 @@ public class GameController : MonoBehaviour {
         {
             GameOver();
         }
+
         if (buttonList [2].text == playerSide && buttonList [4].text == playerSide && buttonList [6].text == playerSide)
         {
             GameOver();
         }
+
+        if (moveCount >= 9)
+        {
+            SetGameOverText ("...DRAW...");
+        }
+
         ChangeSides();
     }
 
@@ -85,7 +95,12 @@ public class GameController : MonoBehaviour {
         {
             buttonList[i].GetComponentInParent<Button>().interactable = false;
         }
+        SetGameOverText (playerSide + " Wins!");
+    }
+
+    void SetGameOverText (string value)
+    {
         gameOverPanel.SetActive(true);
-        gameOverText.text = playerSide + " Wins!";
+        gameOverText.text = value;
     }
 }
