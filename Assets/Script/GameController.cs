2320fdacc97e111e66ce8efda27579fdbec25c76
puthusseri <vyshakputhusseri@gmail.com>
2018-11-25 10:55:35 +0530

    Completed with minimum requirements

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -3,26 +3,27 @@ using UnityEngine.UI;
 using System.Collections;
 
 [System.Serializable]
-public class PlayerClass{
-	
-	public Color PlayerColor;
-	public Color textColor;
-	
-	
-	
-	//Start here........................
-}
-[System.Serializable]
 public class Player {
-	public Image pannel;
-	public Text text;
+   public Image panel;
+   public Text text;
 }
+
+[System.Serializable]
+public class PlayerColor {
+   public Color panelColor;
+   public Color textColor;
+}
+
 public class GameController : MonoBehaviour {
 
    public Text[] buttonList;
    public GameObject gameOverPanel;
    public Text gameOverText;
    public GameObject restartButton;
+   public Player playerX;
+   public Player playerO;
+   public PlayerColor activePlayerColor;
+   public PlayerColor inactivePlayerColor;
 
    private string playerSide;
    private int moveCount;
@@ -34,6 +35,7 @@ public class GameController : MonoBehaviour {
        gameOverPanel.SetActive(false);
        moveCount = 0;
        restartButton.SetActive(false);
+       SetPlayerColors(playerX, playerO);
    }
 
    void SetGameControllerReferenceOnButtons ()
@@ -56,55 +58,64 @@ public class GameController : MonoBehaviour {
        if (buttonList [0].text == playerSide && buttonList [1].text == playerSide && buttonList [2].text == playerSide)
        {
            GameOver(playerSide);
-       }
-
-       if (buttonList [3].text == playerSide && buttonList [4].text == playerSide && buttonList [5].text == playerSide)
+       } 
+       else if (buttonList [3].text == playerSide && buttonList [4].text == playerSide && buttonList [5].text == playerSide)
        {
            GameOver(playerSide);
-       }
-
-       if (buttonList [6].text == playerSide && buttonList [7].text == playerSide && buttonList [8].text == playerSide)
+       } 
+       else if (buttonList [6].text == playerSide && buttonList [7].text == playerSide && buttonList [8].text == playerSide)
        {
            GameOver(playerSide);
-       }
-
-       if (buttonList [0].text == playerSide && buttonList [3].text == playerSide && buttonList [6].text == playerSide)
+       } 
+       else if (buttonList [0].text == playerSide && buttonList [3].text == playerSide && buttonList [6].text == playerSide)
        {
            GameOver(playerSide);
-       }
-
-       if (buttonList [1].text == playerSide && buttonList [4].text == playerSide && buttonList [7].text == playerSide)
+       } 
+       else if (buttonList [1].text == playerSide && buttonList [4].text == playerSide && buttonList [7].text == playerSide)
        {
            GameOver(playerSide);
-       }
-
-       if (buttonList [2].text == playerSide && buttonList [5].text == playerSide && buttonList [8].text == playerSide)
+       } 
+       else if (buttonList [2].text == playerSide && buttonList [5].text == playerSide && buttonList [8].text == playerSide)
        {
            GameOver(playerSide);
-       }
-
-       if (buttonList [0].text == playerSide && buttonList [4].text == playerSide && buttonList [8].text == playerSide)
+       } 
+       else if (buttonList [0].text == playerSide && buttonList [4].text == playerSide && buttonList [8].text == playerSide)
        {
            GameOver(playerSide);
-       }
-
-       if (buttonList [2].text == playerSide && buttonList [4].text == playerSide && buttonList [6].text == playerSide)
+       } 
+       else if (buttonList [2].text == playerSide && buttonList [4].text == playerSide && buttonList [6].text == playerSide)
        {
            GameOver(playerSide);
-       }
-
-       if (moveCount >= 9)
+       } 
+       else if (moveCount >= 9)
        {
            GameOver("draw");
+       } 
+       else
+       {
+           ChangeSides();
        }
-
-       ChangeSides();
-
    }
 
    void ChangeSides ()
    {
        playerSide = (playerSide == "X") ? "O" : "X";
+       if (playerSide == "X")
+       {
+           SetPlayerColors(playerX, playerO);
+       } 
+       else
+       {
+           SetPlayerColors(playerO, playerX);
+       }
+   }
+
+   void SetPlayerColors (Player newPlayer, Player oldPlayer)
+   {
+       newPlayer.panel.color = activePlayerColor.panelColor;
+       newPlayer.text.color = activePlayerColor.textColor;
+       oldPlayer.panel.color = inactivePlayerColor.panelColor;
+       oldPlayer.text.color = inactivePlayerColor.textColor;
    }
 
    void GameOver (string winningPlayer)
@@ -132,6 +143,7 @@ public class GameController : MonoBehaviour {
        moveCount = 0;
        gameOverPanel.SetActive(false);
        restartButton.SetActive(false);
+       SetPlayerColors(playerX, playerO);
        SetBoardInteractable(true);
 
        for (int i = 0; i < buttonList.Length; i++)
